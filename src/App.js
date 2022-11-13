14b4b8f1a862f585fec50387dfe28798efccac38
Srishu-21 <bellpeppers.and.beef.26@gmail.com>
2022-11-12 21:15:20 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import GoogleMap from './GoogleMap';
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+      <GoogleMap />
+    </>
+  )
+}
+
+export default App;
